aa96aaaf84099b241625461188e160e93a45056d
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-11-24 16:02:23 +0800

    在收缩状态的侧边栏点击叶子菜单后自动隐藏该二级菜单组

--- static/src/js/ready_init.js
+++ static/src/js/ready_init.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
     //$('.hide_secondary').hide();
     $('.oe_secondary_menu_section').click(function(e) {
-        if (e.target.nodeName == 'A')return;
+        if (e.target.nodeName == 'A')return;//二级菜单无子菜单时
         if($(this).next('.hide_secondary').length==0){
             //e.preventDefault();
             //e.stopPropagation();
             $(this).find('a').click();
         }
         if ($(this).attr('class') == 'oe_secondary_menu_section open_menu') {
+            //点击二级菜单关闭其子菜单
             $(this).removeClass('open_menu');
             //$('.oe_secondary_menu_section').removeClass('open_menu');
             $(this).next('.hide_secondary').hide();
         } else {
+            //点击缩小的二级菜单时默认关闭此菜单及其子菜单的显示
             if($('body').hasClass('oe_leftbar_open')){
                 $('.oe_secondary_menu_section').removeClass('open_menu');
                 $('.hide_secondary').hide();
             }
             next_element = $(this).next();
+            //点击二级菜单展开其子菜单
             if (next_element && next_element.attr('class') && next_element.attr('class') == 'oe_secondary_submenu nav nav-pills nav-stacked hide_secondary') {
                 next_element.show();
                 $(this).addClass('open_menu');
@@ -24,6 +27,7 @@ $(document).ready(function() {
         }
     });
     $('.oe_application_menu_placeholder').click(function() {
+        // 当点击主菜单时
         //$('.hide_secondary').css('display', 'none');
     })
 
@@ -38,4 +42,11 @@ $(document).ready(function() {
         }
     });
 */
+    $('ul.oe_secondary_submenu a.oe_menu_leaf').click(function() {
+        //点击非二级菜单的叶子菜单a
+        if($('body').hasClass('oe_leftbar_open')){
+            $('.oe_secondary_menu_section').removeClass('open_menu');
+            $('.hide_secondary').hide();
+        }
+    })
 });
